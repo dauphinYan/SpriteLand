75944177d8268ac7c09c33808a4fea8b9f113bef
dauphinYan <584485321@qq.com>
2025-05-21 20:09:56 +0800

    Update.

--- Source/SpriteLand/Character/Type/Hero/Component/Mage/MageCombatComponent.h
+++ Source/SpriteLand/Character/Type/Hero/Component/Mage/MageCombatComponent.h
@@ -1,17 +1,15 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Character/Type/Hero/Component/HeroCombatComponent.h"
+#include "../HeroCombatComponent.h"
 #include "MageCombatComponent.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class SPRITELAND_API UMageCombatComponent : public UHeroCombatComponent
 {
 	GENERATED_BODY()
-	
+
+protected:
+	virtual void Attack() override;
 };
